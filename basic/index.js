8343d3ceea5405f3a80b24ff21e1ac2fb5a809ba
Fernando Nazario <fernandonazario4@gmail.com>
2022-03-29 10:01:06 -0300

    displaying calcs in html body

--- basic/index.js
+++ basic/index.js
@@ -1,23 +1,24 @@
-function sum(number1, number2) {
-  let total = number1 + number2
-  return `A soma dos dois números ${number1} + ${number2} e: ${total}`
-}
-console.log(sum(3, 6))
+let contas = [
+  function sum(number1, number2) {
+    let total = number1 + number2
+    return `A soma dos dois números ${number1} + ${number2} e: ${total}`
+  },
+  function division(number1, number2) {
+    let total = number1 / number2
+    return `A divisão dos dois números ${number1} / ${number2} e: ${total}`
+  },
+  function multiplication(number1, number2) {
+    let total = number1 * number2
+    return `A multiplicação dos dois números ${number1} * ${number2} e: ${total}`
+  },
+  function subtraction(number1, number2) {
+    let total = number1 - number2
+    return `A multiplicação dos dois números ${number1} - ${number2} e: ${total}`
+  }
+]
 
-function division(number1, number2) {
-  let total = number1 / number2
-  return `A divisão dos dois números ${number1} / ${number2} e: ${total}`
-}
-console.log(division(3, 6))
-
-function multiplication(number1, number2) {
-  let total = number1 * number2
-  return `A multiplicação dos dois números ${number1} * ${number2} e: ${total}`
-}
-console.log(multiplication(3, 6))
-
-function subtraction(number1, number2) {
-  let total = number1 - number2
-  return `A multiplicação dos dois números ${number1} - ${number2} e: ${total}`
-}
-console.log(subtraction(6, 6))
+contas.forEach(item => {
+  let p = document.createElement('p')
+  p.innerHTML = item(6, 5)
+  document.body.appendChild(p)
+})
